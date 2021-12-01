cfbbd44e9f9b7eec65cf2813d998e47b336547f1
MagdalenaVasile <92403839+MagdalenaVasileva@users.noreply.github.com>
2021-12-01 14:20:45 +0000

    Files updated

--- src/exercise7/TimeValidation.java
+++ src/exercise7/TimeValidation.java
@@ -3,28 +3,28 @@ package exercise7;
 import java.util.Scanner;
 
 public class TimeValidation {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter hours: ");
-        int hours = input.nextInt();
+    System.out.print("Enter hours: ");
+    int hours = input.nextInt();
 
-        System.out.print("Enter minutes: ");
-        int minutes = input.nextInt();
+    System.out.print("Enter minutes: ");
+    int minutes = input.nextInt();
 
-        boolean timeValid = false; // Call validation methods
-        System.out.println("Time valid: " + timeValid);
+    boolean timeValid = false;
+    timeValid = areHoursValid(hours);
+    if (timeValid) {
+      timeValid = areMinutesValid(minutes);
     }
+    System.out.println("Time valid: " + timeValid);
+  }
 
+  public static boolean areHoursValid(int hours) {
+    return hours < 24 && hours >= 0;
+  }
 
-    public static boolean areHoursValid(int hours) {
-        // Implement method
-        return false;
-    }
-
-
-    public static boolean areMinutesValid(int minutes) {
-        // Implement method
-        return false;
-    }
+  public static boolean areMinutesValid(int minutes) {
+    return minutes < 60 && minutes >= 0;
+  }
 }
